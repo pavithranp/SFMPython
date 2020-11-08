29fcb85428dc6355e106ccdd425b55a4bd34ca81
pavithranp <ptpn94@gmail.com>
2020-11-08 22:35:35 +0100

    3 view reconstruction

--- reconstruction/Classic_SFM_python/SFM.py
+++ reconstruction/Classic_SFM_python/SFM.py
@@ -1,80 +1,92 @@
 import cv2 as cv
 import numpy as np
-from Classic_SFM_python.util import triangulate, essentialMatrix,points_3d_visualize,cameraPose,keypointColor
-#camera intrinsics
+from Classic_SFM_python.util import triangulate, essentialMatrix, points_3d_visualize, cameraPose, keypointColor
+import os
+# camera intrinsics
 f = 720
 
-k =np.eye(3)
+k = np.eye(3)
 k[0][0] = f
 k[1][1] = f
 k[0][2] = 640
-k[1][2] = 390
+k[1][2] = 360
 
-img1 = cv.imread('example_data/vlcsnap-2020-10-26-10h32m50s487.png', 1)  #queryimage # left image
-img2 = cv.imread('example_data/vlcsnap-2020-10-26-10h32m56s617.png', 1) #trainimage # right image
-sift = cv.xfeatures2d.SIFT_create()
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-# FLANN parameters
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks=50)
-flann = cv.FlannBasedMatcher(index_params,search_params)
-matches = flann.knnMatch(des1,des2,k=2)
-good = []
-pts1 = []
-pts2 = []
-# ratio test as per Lowe's paper
-for i,(m,n) in enumerate(matches):
-    if m.distance < 0.8*n.distance:
-        good.append(m)
-        pts2.append(kp2[m.trainIdx].pt)
-        pts1.append(kp1[m.queryIdx].pt)
+prev_cam = np.array([[1, 0, 0, 0],
+                     [0, 1, 0, 0],
+                     [0, 0, 1, 0]])
 
+dir = 'example_data/hilltemple/'
+images = os.listdir(dir)
 
-pts1 = np.int32(pts1)
-pts2 = np.int32(pts2)
-F, mask = cv.findFundamentalMat(pts1,pts2,cv.FM_LMEDS)
-# We select only inlier points
-pts1 = pts1[mask.ravel()==1]
-pts2 = pts2[mask.ravel()==1]
+# images = ['vlcsnap-2020-10-26-10h32m43s388.png',
+#           'vlcsnap-2020-10-26-10h32m50s487.png', 'vlcsnap-2020-10-26-10h32m56s617.png']
+keypoints = np.empty((0, 3))
+pointcolor = np.empty((0, 3))
+for i in range(len(images) - 1):
+    print("matching ",images[i],"and",images[i + 1])
+    img1 = cv.imread(dir + images[i], 1)  # queryimage # left image
+    img2 = cv.imread(dir + images[i + 1], 1)  # trainimage # right image
+    sift = cv.xfeatures2d.SIFT_create()
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+    good = []
+    pts1 = []
+    pts2 = []
+    # ratio test as per Lowe's paper
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.9 * n.distance:
+            good.append(m)
+            pts2.append(kp2[m.trainIdx].pt)
+            pts1.append(kp1[m.queryIdx].pt)
 
-dst = cv.addWeighted(img1,0.5,img2,0.5,5)
+    pts1 = np.int32(pts1)
+    pts2 = np.int32(pts2)
+    F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_LMEDS)
+    # We select only inlier points
+    pts1 = pts1[mask.ravel() == 1]
+    pts2 = pts2[mask.ravel() == 1]
+    print("     found ",len(pts1)," points")
+    dst = cv.addWeighted(img1, 0.5, img2, 0.5, 5)
 
-E = essentialMatrix(F, k)
-# CALCULATE M1 and M2
-M1 = np.array([[1, 0, 0, 0],
-               [0, 1, 0, 0],
-               [0, 0, 1, 0]])
-M2_list = cameraPose(E)
-#  TRIANGULATION
-C1 = k.dot(M1)
+    E = essentialMatrix(F, k)
+    # CALCULATE M1 and M2
+    M1 = prev_cam
+    M2_list = cameraPose(E)
+    #  TRIANGULATION
+    C1 = k.dot(M1)
 
-P_best = np.zeros((pts1.shape[0], 3))
-M2_best = np.zeros((3, 4))
-C2_best = np.zeros((3, 4))
-err_best = np.inf
-error_list = []
+    P_best = np.zeros((pts1.shape[0], 3))
+    M2_best = np.zeros((3, 4))
+    C2_best = np.zeros((3, 4))
+    err_best = np.inf
+    error_list = []
 
-# get_color
-colors = keypointColor(img1,img2,pts1,pts2)
+    # get_color
+    colors = keypointColor(img1, img2, pts1, pts2)
 
-for i in range(M2_list.shape[2]):
-    M2 = M2_list[:, :, i]
-    C2 = k.dot(M2)
-    P_i, err = triangulate(C1, pts1, C2, pts2)
-    error_list.append(err)
-    z_list = P_i[:, 2]
-    if all(z > 0 for z in z_list):
-        err_best = err
-        P_best = P_i
-        M2_best = M2
-        C2_best = C2
-
-
-
-points_3d_visualize(P_best,colors)
+    for i in range(M2_list.shape[2]):
+        M2 = M2_list[:, :, i]
+        C2 = k.dot(M2)
+        P_i, err = triangulate(C1, pts1, C2, pts2)
+        error_list.append(err)
+        z_list = P_i[:, 2]
+        if all(z > 0 for z in z_list):
+            err_best = err
+            P_best = P_i
+            M2_best = M2
+            C2_best = C2
+    # keypoints.append(P_best)
+    keypoints = np.vstack((keypoints, P_best))
+    pointcolor = np.vstack((pointcolor, colors))
+    prev_cam = M2_best
+points_3d_visualize(keypoints, pointcolor,s=1.7)
 
 # for x,y in zip(pts1,pts2):
 #     # dst = cv.line(dst, tuple(x),tuple(y) , (0,255,0), 1)
@@ -90,7 +102,3 @@ points_3d_visualize(P_best,colors)
 #         break
 #     elif k==-1:  # normally -1 returned,so don't print it
 #         continue
-
-
-
-
--- reconstruction/Classic_SFM_python/util.py
+++ reconstruction/Classic_SFM_python/util.py
@@ -112,7 +112,7 @@ def triangulate(C1, pts1, C2, pts2):
     return P_i, reprojection_err
 
 
-def points_3d_visualize(P_best,colors):
+def points_3d_visualize(P_best,colors,s):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.set_aspect('auto')
@@ -121,15 +121,15 @@ def points_3d_visualize(P_best,colors):
     Y = P_best[:, 1]
     Z = P_best[:, 2]
 
-    ax.scatter(X, Y, Z, s=5,c=colors/255.0)
+    ax.scatter(X, Y, Z, s=s,c=colors/255.0)
 
     max_range = np.array([X.max() - X.min(), Y.max() - Y.min(), Z.max() - Z.min()]).max() / 2.0
 
     mid_x = (X.max() + X.min()) * 0.5
     mid_y = (Y.max() + Y.min()) * 0.5
     mid_z = (Z.max() + Z.min()) * 0.5
-    ax.set_xlim(mid_x - max_range, mid_x + max_range)
-    ax.set_ylim(mid_y - max_range, mid_y + max_range)
-    ax.set_zlim(mid_z - max_range, mid_z + max_range)
+    # ax.set_xlim(mid_x - max_range, mid_x + max_range)
+    # ax.set_ylim(mid_y - max_range, mid_y + max_range)
+    # ax.set_zlim(mid_z - max_range, mid_z + max_range)
 
     plt.show()
